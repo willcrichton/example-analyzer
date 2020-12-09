9dc1b4abc16321084c86b505c3c7d239c1e7b685
Will Crichton <wcrichto@cs.stanford.edu>
2020-12-09 12:21:17 -0500

    Invert call locations map

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
 #![feature(rustc_private)]
 
 use std::collections::HashMap;
-use rustc_data_structures::fx::{FxHashSet, FxHashMap};
-use rustc_session::config::Options;
-use std::path::PathBuf;
 use rustc_hir::{intravisit::{self, Visitor}, self as hir};
 use rustc_middle::ty::{TyKind, TyCtxt};
 use rustc_middle::hir::map::Map;
-use rustc_serialize::{Encoder, Encodable};
 
 extern crate rustc_data_structures;
 extern crate rustc_session;
@@ -18,25 +14,12 @@ extern crate rustc_hir;
 extern crate rustc_middle;
 extern crate rustc_serialize;
 
-type CallLocations = HashMap<String, Vec<(u32, u32)>>;
-
-// struct CallLocations {
-//     file: String,
-//     locations: Locations
-// }
-
-// impl<S: Encoder> Encodable<S> for CallLocations {
-//     fn encode(&self, s: &mut S) -> Result<(), S::Error> {
-//         s.emit_struct("CallLocations", 2, |s| {
-//             s.emit_struct_field("file", 0, |s| self.file.encode(s))?;
-//             s.emit_struct_field("locations", 1, |s| self.locations.encode(s))
-//         })
-//     }
-// }
+type CallLocations = HashMap<String, HashMap<String, Vec<(u32, u32)>>>;
 
 struct FindCalls<'a, 'tcx> {
     tcx: TyCtxt<'tcx>,
     map: Map<'tcx>,
+    file_name: String,
     calls: &'a mut CallLocations
 }
 
@@ -62,24 +45,27 @@ impl<'a, 'tcx> Visitor<'tcx> for FindCalls<'a, 'tcx> where 'tcx: 'a {
         if let Some(ty) = ty {
             if let TyKind::FnDef(def_id, _) = ty.kind() {
                 let key = self.tcx.def_path_str(*def_id);
-                self.calls.entry(key).or_insert(vec![]).push((ex.span.lo().0, ex.span.hi().0));
+                let entries = self.calls.entry(key).or_insert(HashMap::new());
+                entries.entry(self.file_name.clone()).or_insert(vec![]).push((ex.span.lo().0, ex.span.hi().0));
             }
         }
         intravisit::walk_expr(self, ex);
     }
 }
 
-struct Callbacks {
-    calls: CallLocations
+struct Callbacks<'a> {
+    file_name: String,
+    calls: &'a mut CallLocations
 }
 
-impl rustc_driver::Callbacks for Callbacks {
+impl<'a> rustc_driver::Callbacks for Callbacks<'a> {
     fn after_analysis<'tcx>(&mut self, _compiler: &rustc_interface::interface::Compiler, queries: &'tcx rustc_interface::Queries<'tcx>) -> rustc_driver::Compilation {        
         queries.global_ctxt().unwrap().take().enter(|tcx| {
             let krate = tcx.hir().krate();
             let map = tcx.hir();
-            let mut calls = &mut self.calls;
-            let mut finder = FindCalls { calls, tcx, map };
+            let calls = &mut self.calls;
+            let file_name = self.file_name.clone();
+            let mut finder = FindCalls { calls, tcx, map, file_name };
             krate.visit_all_item_likes(&mut finder.as_deep_visitor());
         });
 
@@ -92,13 +78,6 @@ use regex::Regex;
 use std::fs;
 
 fn main() {
-    // let input = r#"fn main() { 
-    //     let s = String::from("a");
-    //     let mut v: Vec<i32> = Vec::new();
-    //     v.push(1);
-    //  }"#
-    //     .to_string();    
-
     Command::new("cargo")
         .args(&["clean", "--target-dir", "target/debug/examples"])
         .output().unwrap();
@@ -109,26 +88,25 @@ fn main() {
     let stderr = String::from_utf8(stderr).unwrap();
 
     let re = Regex::new(r"^\s*Running `(.*)`").unwrap();
-    let commands: HashMap<_, _> = stderr
+    let mut calls = HashMap::new();
+    stderr
         .split("\n")
         .filter_map(|line| {
             re.captures(line).map(|cap| cap.get(1).unwrap().as_str().to_string())
         })
-        .map(|command| {
+        .for_each(|command| {
             let args: Vec<_> = command
                 .split(" ")
                 .chain(vec!["--sysroot", "/Users/will/.rustup/toolchains/nightly-x86_64-apple-darwin"].into_iter())
                 .map(|s| s.to_string())
                 .collect();
-            let file = &args[4];            
-            let mut callbacks = Callbacks { calls: HashMap::new() };
+            let file_name = args[4].to_string();     
+                   
+            let mut callbacks = Callbacks { calls: &mut calls, file_name };
             let runner = rustc_driver::RunCompiler::new(&args, &mut callbacks);
             runner.run().unwrap();
+        });
 
-            (file.to_string(), callbacks.calls)
-        })
-        .collect();
-
-    let json = rustc_serialize::json::encode(&commands).unwrap();
+    let json = rustc_serialize::json::encode(&calls).unwrap();
     fs::write(".call_locations.json", json).unwrap();
 }
